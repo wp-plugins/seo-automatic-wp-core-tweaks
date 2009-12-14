e18dcfbaff1b746adc1afb6eb7eff3cc2189f398
cyber49 <cyber49@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-12-14 19:25:30 +0000

    git-svn-id: https://plugins.svn.wordpress.org/seo-automatic-wp-core-tweaks/trunk@182813 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- d-comment.php
+++ d-comment.php
@@ -6,7 +6,7 @@ if ($_REQUEST['action'] == "changecore") {
 	$post = & get_post($post_id);
 	if ($_REQUEST['d_comment'] == "ON") {
 		if ( !wp_delete_comment($post_id) ) {
-			$fail .= "<li>Default comment could not be deleted. Perhaps it was already removed?</li>";
+			$fail .= "<li>Default comment could not be deleted. Perhaps it was already removed or did not exist in the initial wordpress install?</li>";
 		} else {
 			$success .= "<li>Default comment deleted.</li>";
 		}
@@ -14,6 +14,6 @@ if ($_REQUEST['action'] == "changecore") {
 		$notused .= "<li>Default comment removal.</li>";
 	}
 } else {
-	$options .= "<li><input type=\"checkbox\" name=\"d_comment\" value=\"ON\" checked> Delete default comment.</li>";
+	$options .= "<li><input type=\"checkbox\" name=\"d_comment\" value=\"ON\" checked> Delete default comment. (Wordpress has not been adding the sample comment in recent versions, so if you choose this option, the result screen may show that the comment was not deleted but perhaps already had been.)</li>";
 }
 ?>
