dad48fd042702ec61ba857e53fef2987e2f1514d
cyber49 <cyber49@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-05 18:58:50 +0000

    git-svn-id: https://plugins.svn.wordpress.org/seo-automatic-wp-core-tweaks/trunk@213618 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- cat-names.php
+++ cat-names.php
@@ -38,7 +38,7 @@ if ($_REQUEST['action'] == "changecore") {
 		if ( wp_update_category($default_cat_setting) )
 			$success .= "<li>Default link category is set to ".$cat_2."</li>";
 		else
-			$fail .= "<li>Default category is not changed. You either do not have sufficient permission as an admin user or an error has occured.</li>";
+			//$fail .= "<li>Default category is not changed. You either do not have sufficient permission as an admin user or an error has occured.</li>";
 	} else {
 		$notused .= "<li>Change default blogroll category name.</li>";
 	} 
