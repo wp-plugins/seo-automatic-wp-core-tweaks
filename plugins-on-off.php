cb658c04f0d659e0f7f98424f20bdf7851216e41
Dzynit <Dzynit@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-22 16:31:49 +0000

    git-svn-id: https://plugins.svn.wordpress.org/seo-automatic-wp-core-tweaks/tags/3.7.2@413810 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ plugins-on-off.php
@@ -0,0 +1,106 @@
+<?php
+if ($_REQUEST['action'] != "pluginsonoff") { $options2 = '<ul><li>These four plugins have been hugely instrumental in the development of wordPress, and we were happy to have collaborated with the authors of these great plugins to include them for you. The default installation is set to off, but we do recommend them.</li><li>&nbsp;</li><li><b style="color: #009900;">
+If you have upgraded Core Tweaks and this is not a first time install, the plugins that are noted "off by default" will remain turned on. This ensures that your settings will not be disrupted. If it is the first installation for you, then the default will remain "off".</b></li><li>&nbsp;</li>'; }
+
+// Turn post teaser on and off
+if ($_REQUEST['action'] == "pluginsonoff") {
+	if ($_REQUEST['post_teaser_use'] == "on") {
+		if (!update_option('seoauto_core_post_teaser_use', 'on')) {
+			$fail .= "<li>Post teaser could not be turned on. Maybe it's already on.</li>";
+		} else {
+			$success .= "<li>Post Teaser is turned on.</li>";
+		}
+	} elseif ($_REQUEST['post_teaser_use'] == "off") {
+		if (!update_option('seoauto_core_post_teaser_use', 'off')) {
+			$fail .= "<li>Post Teaser could not be turned off. Maybe it's already off.</li>";
+		} else {
+			$success .= "<li>Post Teaser is turned off.</li>";
+		}
+	} else {
+		$notused .= "<li>Post Teaser is off by default.</li>";
+	}
+} else {
+	if (get_option('seoauto_core_post_teaser_use') != 'off') {
+		$link = '<a href="options-general.php?page=seo-automatic-wp-core-tweaks/post-teaser.php">';
+		$endlink = '</a>';
+	}
+	$options2 .= '<li><a href="http://wordpress.org/extend/plugins/post-teaser/" target="_blank">Post Teaser:</a> <input type="radio" name="post_teaser_use" value="on"> On <input type="radio" name="post_teaser_use" value="off" checked> Off <small><b>(Off by default.)</b></small> <a href="options-general.php?page=seo-automatic-wp-core-tweaks/post-teaser.php">See the new post teaser options.</a><br />Post Teaser generates excerpts of your posts for the main, archive, and category pages. It also generates a line showing the estimated word count, image count, and reading time. These are all fully editable from the admin.<br />Access under Settings > '.$link.'Post Teaser'.$endlink.'</li><li>&nbsp;</li>';
+}
+
+// Turn page link on and off
+if ($_REQUEST['action'] == "pluginsonoff") {
+	if ($_REQUEST['page_link_use'] == "on") {
+		if (!update_option('seoauto_core_page_link_use', 'on')) {
+			$fail .= "<li>Page Link could not be turned on. Maybe it's already on.</li>";
+		} else {
+			$success .= "<li>Page Link is turned on.</li>";
+		}
+	} elseif ($_REQUEST['page_link_use'] == "off") {
+		if (!update_option('seoauto_core_page_link_use', 'off')) {
+			$fail .= "<li>Page Link could not be turned off. Maybe it's already off.</li>";
+		} else {
+			$success .= "<li>Page Link is turned off.</li>";
+		}
+	} else {
+		$notused .= "<li>Page Link is off by default.</li>";
+	}
+} else {
+	if (get_option('seoauto_core_page_link_use') != 'off') {
+		$link2 = '<a href="tools.php?page=seo-automatic-wp-core-tweaks/page-link.php">';
+		$endlink2 = '</a>';
+	}
+	$options2 .= '<li><a href="http://gmurphey.com/2006/10/05/wordpress-plugin-page-link-manager/" target="_blank">Page Link:</a> <input type="radio" name="page_link_use" value="on"> On <input type="radio" name="page_link_use" value="off" checked> Off <small><b>(Off by default.)</b></small><br />Exclude any pages you wish from main navigation. Still amazing for older style themes, this plugin is obsolete for 2011, and the WordPress Twenty-Ten default theme. Instead, use the built in WordPress custom menu options.<br />Access under Tools > '.$link2.'Page Links'.$endlink2.' and check / uncheck as desired.</li><li>&nbsp;</li>';
+}
+
+// Turn page order on and off
+if ($_REQUEST['action'] == "pluginsonoff") {
+	if ($_REQUEST['page_order_use'] == "on") {
+		if (!update_option('seoauto_core_page_order_use', 'on')) {
+			$fail .= "<li>Page Order could not be turned on. Maybe it's already on.</li>";
+		} else {
+			$success .= "<li>Page Order is turned on.</li>";
+		}
+	} elseif ($_REQUEST['page_order_use'] == "off") {
+		if (!update_option('seoauto_core_page_order_use', 'off')) {
+			$fail .= "<li>Page Order could not be turned off. Maybe it's already off.</li>";
+		} else {
+			$success .= "<li>Page Order is turned off.</li>";
+		}
+	} else {
+		$notused .= "<li>Page Order is off by default.</li>";
+	}
+} else {
+	if (get_option('seoauto_core_page_order_use') != 'off') {
+		$link3 = '<a href="edit.php?post_type=page&page=mypageorder">';
+		$endlink3 = '</a>';
+	}
+	$options2 .= '<li><a href="http://wordpress.org/extend/plugins/my-page-order/" target="_blank">Page Order:</a> <input type="radio" name="page_order_use" value="on"> On <input type="radio" name="page_order_use" value="off" checked> Off <small><b>(Off by default.)</b></small><br />Again, an amazing plugin that was ahead of its time, made obsolete buy custom menus. For themes that dont support them, try this!<br />Access under Pages > '.$link3.'My Page Order'.$endlink3.'</li></li>&nbsp;</li></li>';
+}
+
+// Turn Sitemap Generator on and off
+if ($_REQUEST['action'] == "pluginsonoff") {
+	if ($_REQUEST['dd_sitemap_use'] == "on") {
+		if (!update_option('seoauto_core_dd_sitemap_use', 'on')) {
+			$fail .= "<li>Sitemap Generator could not be turned on. Maybe it's already on.</li>";
+		} else {
+			$success .= "<li>Sitemap Generator is turned on.</li>";
+		}
+	} elseif ($_REQUEST['dd_sitemap_use'] == "off") {
+		if (!update_option('seoauto_core_dd_sitemap_use', 'off')) {
+			$fail .= "<li>Sitemap Generator could not be turned off. Maybe it's already off.</li>";
+		} else {
+			$success .= "<li>Sitemap Generator is turned off.</li>";
+		}
+	} else {
+		$notused .= "<li>Sitemap Generator is off by default.</li>";
+	}
+} else {
+	if (get_option('seoauto_core_dd_sitemap_use') != 'off') {
+		$link4 = '<a href="options-general.php?page=seo-automatic-wp-core-tweaks/sitemap-generator.php">';
+		$endlink4 = '</a>';
+	}
+	$options2 .= '<li><a href="http://www.dagondesign.com/articles/sitemap-generator-plugin-for-wordpress/" target="_blank">Sitemap Generator:</a> <input type="radio" name="dd_sitemap_use" value="on"> On <input type="radio" name="dd_sitemap_use" value="off" checked> Off <small><b>(Off by default.)</b></small><br/>This plugin creates a static sitemap (not to be confused with an XML sitemap) for your WordPress site. Simply placing the shortcode &lt;!-- ddsitemapgen --&gt; in your .html will show the options you\'ve selected in your admin, so the spiders and bots have a path to crawl each page and post.<br />Access under Settings > '.$link4.'DDSitemapGen'.$endlink4.'</li><li>&nbsp;</li>';
+}
+
+if ($_REQUEST['action'] != "pluginsonoff") { $options2 .= '</ul>'; }
+?>
---
+++ readme.txt
@@ -0,0 +1,106 @@
+=== Core Tweaks WordPress Setup ===
+Contributors: cyber49
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=FC2PBBP6BY8QC
+Tags: google, seo, permalinks, installation, common settings, post teaser, page link, page order, canonical, sitemap, h1, core tweaks, seo automatic, blog setup
+Requires at least: 2.8
+Tested up to: 3.2
+Stable tag: 3.7.2
+
+Primarily for use in new WP installations, the steps are based on an employee checklist from Search Commander, Inc. for setting up sites.
+
+== Description ==
+<a href="http://www.seoautomatic.com/plugins/wp-core-tweaks/" target="_blank">Plugin Page</a> | <a href="http://www.seoautomatic.com/forum/wp-tweak-plugin/" target="_blank">Plugin Support</a>
+
+This plug-in completely automates the proper setup of your WordPress blog and will save 20 to 40 minutes of post-install time.
+
+[youtube http://www.youtube.com/v/UTXrUAccCts&hl=en_US&fs=1&rel=0&color1=0x006699&color2=0x54abd6]
+
+Almost everything you would do manually is covered, from changing permalink structure to deleting the Hello World sample post. All the options are accessible from one single page.
+
+It also extends the built-in features of WordPress and combines 4 other useful plugins into this one.
+
+This plug-in is designed to be a companion to other plugins like All in one SEO, Headspace, or WordPress SEO, and *is not* intended as a replacement. In fact, as they get better, we have disabled some of our features. 
+
+= Features =
+From one admin screen, click a button and make any or all of these changes...
+
+1.	Change default post category name from "uncategorized" to: 
+2.	Change default blogroll category name from "blogroll" to: 
+3.	Turn OFF organization of uploads into month and year-based folders.
+4.	Set the RSS feed to display only a summary.
+5.	Change permalink structure to custom setting: 
+6.	Add a page and shortcode for the integrated DDSiteMap Generator
+7.	Create a privacy page with your contact information
+8.	Add a Contact Page 
+9.	Delete default "Hello World" post.
+10.	Select a page to be displayed as your home page
+11.	Change default post comments on articles.
+12.	Delete default comment
+13.	Set the main blog email
+14.	Set the admin user's blog email
+15.	Add new users
+16.	Change the default page name from "about"
+17.	Set the default page as the front page.
+18.	Create the page to designate for displaying your blog posts, and name it: 
+19.	Change the blog description tagline. 
+20.	Delete all links in blogroll.
+21.	Enable the Atom Publishing Protocol.
+22.	Enable the WordPress, Movable Type, MetaWeblog and Blogger XML-RPC publishing protocols.
+23.	Add a robots.txt file to your blog root.(This WILL overwrite any existing robots.txt file and cannot be undone.)
+24.	Footer control
+	*	Add a copyright date to the footer.
+	*	Add an admin login to the footer.
+	*	Add the static sitemap link to the footer.
+	*	Add the privacy policy link to the footer.
+25.	Advanced:
+26.	Uploads Folder: You will need to have writable permission
+27.	Attempt to create the uploads folder and set the permission to writable.
+28.	Change H2 tags to H1
+
+== Installation ==
+
+After activation, go to the SEO Automatic > Core Tweaks admin screen and use the default choices or make choices of your own. 
+
+Designed as a new setup assistant, this plug-in is usually installed before any others. If you've added other plugins and have them actice, deactivate them first to avoid potential conflicts, then REactivvate them after you run the setup process. 
+
+[youtube http://www.youtube.com/v/UTXrUAccCts&hl=en_US&fs=1&rel=0&color1=0x006699&color2=0x54abd6]
+
+== Frequently Asked Questions ==
+
+= What is this plugin for? =
+
+This plugin saves 20 to 40 minutes of post-installation time, properly setting up a new WordPress installation. 
+
+It adds a new Admin menu item, SEO Automatic - with a sub-menu Core Tweaks, giving access on one page to all of the things that savvy programmers change for every installation. 
+
+For older themes that do not support custom menus, it also combines several other WP plugins for theme customization, turned off by default in the bottom of the admin screen.
+
+= What if I already have a Wordpress site up and running? =
+
+Then this plugin won't save you nearly as much time unless you've done little customization after your installation. Deactivate any plugins that are turned on, and be sure to UNcheck any options in the admin panel that you may have already completed, or that you don't need to change.
+
+= Does core tweaks work with Windows servers? =
+
+No - Windows servers are not currently supported, and never will be.
+
+= What if I need more help? =
+Please visit the <a href='http://www.seoautomatic.com/forum/wp-tweak-plugin/'>SEO Automatic Support Forum</a>, which should be pretty empty, since we have very few issues.
+
+= Help! I have a missing/broken image on the privacy policy page created by Core Tweaks =
+
+This has been corrected starting with version 3.71. The previous image supplied by freeprivacypolicy.com became unavailable and we didn't notice. 
+
+If you have not edited your privacy policy after creation, either delete it and and use core tweaks to add one again after you have upgraded to 3.71 or higher, or much easier, manually edit your page to remove the broken image.
+
+== Screenshots ==
+
+1. Screenshot
+
+== Other Notes ==
+
+1. We recommend disabling any other plugins before running the process to minimize potential confilcts. There will be no conflicts when you re-enable them.
+
+2. Your .htaccess file should be writable for your permalinks to change.
+
+<a href='http://www.seoautomatic.com/plugins/'>SEO Automatic Plugins</a> | 
+<a href='http://www.seoautomatic.com/plugins/wp-core-tweaks/'>SEO Automatic Core Tweaks</a>
